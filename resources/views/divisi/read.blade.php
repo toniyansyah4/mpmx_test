6b9fd7df65227bb88ba4abce9c9b7d88e942d960
tonilenna <toni@lenna.ai>
2022-10-18 07:04:07 +0700

    tes

--- resources/views/divisi/read.blade.php
+++ resources/views/divisi/read.blade.php
@@ -4,12 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8 pb-2">
-                <a href="{{ route('direktur') }}" class="btn btn-danger">Back</a>
+                <a href="{{ route('divisi') }}" class="btn btn-danger">Back</a>
         </div>
         <div class="col-md-8">
             <div class="mb-3">
-            <label class="form-label">Direktur</label>
-            <input type="text" class="form-control" value="{{ $direktur->name }}" name="name">
+            <label class="form-label">Divisi</label>
+            <input type="text" class="form-control" value="{{ $divisi->name }}" name="name">
             </div>
         </div>
     </div>
